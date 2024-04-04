bbe9f462987971d4bbc5a81faca8924e687f2f6e
test <xufu2017@gmail.com>
2024-04-04 03:28:04 +0100

    udpate

---
+++ PdfRead/Program.cs
@@ -0,0 +1,36 @@
+﻿// See https://aka.ms/new-console-template for more information
+using UglyToad.PdfPig.DocumentLayoutAnalysis.TextExtractor;
+using UglyToad.PdfPig;
+using UglyToad.PdfPig.DocumentLayoutAnalysis.WordExtractor;
+
+Console.WriteLine("Hello, World!");
+string filePath = @"annotations_01.pdf";
+string keyword = "Fillable Fields";
+
+List<string> pagesWithKeyword = FindKeywordInPdf(filePath, keyword);
+
+
+foreach (var page in pagesWithKeyword)
+{
+    Console.WriteLine(page);
+}
+static List<string> FindKeywordInPdf(string filePath, string keyword)
+{
+    List<string> pagesWithKeyword = new List<string>();
+
+    using (PdfDocument document = PdfDocument.Open(filePath))
+    {
+        for (int i = 0; i < document.NumberOfPages; i++)
+        {
+            var page = document.GetPage(i + 1);
+            string pageText = string.Join(" ", page.GetWords().Select(w => w.Text));
+
+            if (pageText.Contains(keyword))
+            {
+                pagesWithKeyword.Add($"Keyword found on page {i + 1}");
+            }
+        }
+    }
+
+    return pagesWithKeyword;
+}
---
+++ PdfText7Read/Program.cs
@@ -0,0 +1,40 @@
+﻿using iText.Forms;
+using iText.Forms.Fields;
+using iText.Kernel.Pdf;
+using iText.Kernel.Pdf.Canvas.Parser.Listener;
+using iText.Kernel.Pdf.Canvas.Parser;
+
+var pdfDoc = new PdfDocument(new PdfReader("annotations_01.pdf"));
+string searchText = "Tick Boxes (multiple options can be selected)";
+for (int page = 1; page <= pdfDoc.GetNumberOfPages(); page++)
+{
+    ITextExtractionStrategy strategy = new SimpleTextExtractionStrategy();
+    String pageContent = PdfTextExtractor.GetTextFromPage(pdfDoc.GetPage(page), strategy);
+
+    if (pageContent.Contains(searchText))
+    {
+        Console.WriteLine($"Text found on page {page}");
+    }
+}
+
+
+
+var form = PdfAcroForm.GetAcroForm(pdfDoc, true);
+var fields = form.GetAllFormFields();
+
+foreach (var fieldName in fields.Keys)
+{
+    var field = fields[fieldName];
+    Console.WriteLine(fieldName + ": " + field.GetValueAsString());
+}
+
+PdfFormField nameField = fields["Name"]; // Use the exact field name as defined in the PDF
+PdfFormField addressField = fields["Address"]; // Use the exact field name as defined in the PDF
+//PdfFormField dateField = fields["Date"]; // Use the exact field name as defined in the PDF
+
+Console.WriteLine("Name: " + nameField.GetValueAsString());
+Console.WriteLine("Address: " + addressField.GetValueAsString());
+//Console.WriteLine("Date: " + dateField.GetValueAsString());
+pdfDoc.Close();
+
+Console.Read();
